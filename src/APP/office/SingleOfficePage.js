b233247949b4c77a33e41cd6dd46e7d7880660f3
umijatovic <umijatovic@itekako.com>
2019-06-03 18:31:48 +0200

    adding singleOfficePage

--- src/APP/office/SingleOfficePage.js
+++ src/APP/office/SingleOfficePage.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import './singleOfficePage.scss'
 
 const SingleOfficePage =  (props) =>{
     let office = props.officeData[props.match.params.number];
     return(
-        <div>
-            <Link to='/'>back</Link>
-            <p>{office.name}</p>
-            <p>{office.description} </p>
+        <div className='singleOfficeDiv'>
+            <div className='mapDiv'>
+                <img id='mapImage' src="https://www.lifewire.com/thmb/ybr1LcG6WKL8RCTZ96up_w-64Ps=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/mapsdirectionspart1-56cd4d395f9b5879cc5bd51a.png" alt=""/>
+            </div>
+            
+            <div className='div1'>
+                {(office.photo) ?  <img src={office.photo} alt=""/> : <p id='singleLogoPic'>{office.name.charAt(0).toUpperCase()} </p>} 
+                <div>
+                    <p>{office.name}</p>
+                    <p>{office.description} </p>
+                </div> 
+            </div>
+            <Link to='/'>&#171;back</Link>
         </div>
     )
 }
 
 export default SingleOfficePage;
+
