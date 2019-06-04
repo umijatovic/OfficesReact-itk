04b0a504e56893979cc2aec12a486a0170b7ed34
umijatovic <umijatovic@itekako.com>
2019-06-04 09:41:55 +0200

    scss changing

--- src/APP/office/SingleOfficePage.js
+++ src/APP/office/SingleOfficePage.js
@@ -7,7 +7,7 @@ const SingleOfficePage =  (props) =>{
     return(
         <div className='singleOfficeDiv'>
             <div className='mapDiv'>
-                <img id='mapImage' src="https://www.lifewire.com/thmb/ybr1LcG6WKL8RCTZ96up_w-64Ps=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/mapsdirectionspart1-56cd4d395f9b5879cc5bd51a.png" alt=""/>
+                <img id='mapImage' src="http://www.expertphp.in/images/articles/ArtImgozwPVl_n-min.png" alt=""/>
             </div>
             
             <div className='div1'>
